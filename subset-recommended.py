9593d8393fb329d99ad6a618819da86ba05a8d00
tc <taylorcassidy64@gmail.com>
2017-07-11 14:00:22 -0500

    updated subset-recommended.py to work with python 3.5

--- subset-recommended.py
+++ subset-recommended.py
@@ -20,31 +20,31 @@ pat = re.compile(r"^(.*?):[0-9:]+:([0-9]+):([0-9]+)%$")
 ou = sys.stdout
 
 for line in gzip.open(sys.argv[1]):
-	line = line.split("\t")
-	phrase, count, used = line[0], line[1], line[2]
-	if used < mincount: continue
-	if len(phrase) < minlen: continue
-	m = pat.match(line[3])
-	if not m:
-		print >>sys.stderr, "Did not match:", line
-		continue
-	trust = float(m.group(3))
-	isexact = not (m.group(2) == '0')
-	if isexact:
-		if trust < mintrustexact: continue
-	else:
-		if trust < mintrust: continue
-	if exactonly and not isexact: continue
-	if len(line) > 4:
-		m2 = pat.match(line[4])
-		if not m2:
-			print >>sys.stderr, "Did not match:", line
-			continue
-		trust2 = float(m2.group(3))
-		if trust2 >= trust - mincontrast:
-			continue
-	ou.write(phrase)
-	ou.write("\t")
-	ou.write(m.group(1))
-	ou.write("\n")
-
+    line = line.decode('utf8')
+    line = line.split('\t')
+    phrase, count, used = line[0], int(line[1]), int(line[2])
+    if used < mincount: continue
+    if len(phrase) < minlen: continue
+    m = pat.match(line[3])
+    if not m:
+        print >> sys.stderr, "Did not match:", line
+        continue
+    trust = float(m.group(3))
+    isexact = not (m.group(2) == '0')
+    if isexact:
+        if trust < mintrustexact: continue
+    else:
+        if trust < mintrust: continue
+    if exactonly and not isexact: continue
+    if len(line) > 4:
+        m2 = pat.match(line[4])
+        if not m2:
+            print >> sys.stderr, "Did not match:", line
+            continue
+        trust2 = float(m2.group(3))
+        if trust2 >= trust - mincontrast:
+            continue
+    ou.write(phrase)
+    ou.write("\t")
+    ou.write(m.group(1))
+    ou.write("\n")
